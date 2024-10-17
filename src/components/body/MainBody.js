aee36da620e4772907f3184d8dbf55075db9bcb9
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2024-10-17 19:07:48 -0300

    bug fix

--- src/components/body/MainBody.js
+++ src/components/body/MainBody.js
@@ -6,10 +6,10 @@ const StyledMainBody = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 100vh;
-  max-width: 100%;
+  height: 100%;
+  width: 100%;
   overflow: hidden;
-  position: relative;
+  position: fixed;
 `;
 
 const MainBody = ({ children }) => {
